1c7f5f791cdf0e2be2a37529dafc1ab1b63f26c0
Andrej Gafrik <agafrik982@gmail.com>
2020-05-16 22:06:47 +0200

    hopefully last commit

--- src/tuning3.py
+++ src/tuning3.py
@@ -26,15 +26,23 @@ class Trees:
         model.fit(self.X_train, np.ravel(self.y_train))
 
         y_1 = model.predict(self.X_valid)
+        y_2 = model.predict(self.X_train)
+        
         mean_squared_error=metrics.mean_squared_error(inv_boxcox(self.y_valid, self.price_lambda),inv_boxcox(y_1, self.price_lambda))
         rmlse = metrics.mean_squared_log_error(inv_boxcox(self.y_valid, self.price_lambda), inv_boxcox(y_1, self.price_lambda))
 
+        mse_train = metrics.mean_squared_error(inv_boxcox(self.y_train, self.price_lambda),inv_boxcox(y_2, self.price_lambda))
+        rmlse_train = metrics.mean_squared_log_error(inv_boxcox(self.y_train, self.price_lambda), inv_boxcox(y_2, self.price_lambda))
+        
         r2_train = model.score(self.X_train, self.y_train)
         r2_test = model.score(self.X_valid, self.y_valid)
         adj_r2_train = 1 - (((1 - r2_train) * (self.X_train.shape[0] - 1)) / (self.X_train.shape[0] - self.X_train.shape[1] - 1))
         adj_r2_test = 1 - (((1 - r2_test) * (self.X_valid.shape[0] - 1)) / (self.X_valid.shape[0] - self.X_valid.shape[1] - 1))
         
         score = {
+            "MSE train": round(mse_train, 2),
+            "RMSE train": round(np.sqrt(mse_train),2),
+            "RMSLE train": round(np.sqrt(rmlse_train),2),
             "MSE": round(mean_squared_error, 2),
             "RMSE": round(np.sqrt(mean_squared_error),2),
             "RMSLE": round(np.sqrt(rmlse),2),
@@ -49,7 +57,7 @@ class Trees:
 
     def grid_search(self, model_func,hyperparams, max_iterations):
         
-        df_results = pd.DataFrame(columns = ['MSE', 'RMSE', 'RMSLE', 'R2_train', 'R2_test', 'Adj_R2_train', 'Adj_R2_test', 'params'],
+        df_results = pd.DataFrame(columns = ['MSE train', 'RMSE train', 'RMSLE train', 'MSE', 'RMSE', 'RMSLE', 'R2_train', 'R2_test', 'Adj_R2_train', 'Adj_R2_test', 'params'],
                                       index = list(range(max_iterations)))
         
         keys, values = zip(*hyperparams.items())
@@ -70,7 +78,7 @@ class Trees:
             if i >= max_iterations-1:
                 break
            
-        df_results['delta_R2'] = abs(df_results["R2_train"]-df_results["R2_test"])
+        df_results['delta_RMSE'] = abs(df_results["RMSE train"]-df_results["RMSE"])
         # Sort with best score on top
         df_results.sort_values(['RMSE', 'R2_test'], ascending = [True, False], inplace = True)
         df_results.reset_index(inplace=True)
@@ -80,7 +88,7 @@ class Trees:
 
     def random_search(self, model_func, hyperparams, max_iterations):
         
-        df_results = pd.DataFrame(columns = ['MSE', 'RMSE', 'RMSLE', 'R2_train', 'R2_test', 'Adj_R2_train', 'Adj_R2_test', 'params'],
+        df_results = pd.DataFrame(columns = ['MSE train', 'RMSE train', 'RMSLE train', 'MSE', 'RMSE', 'RMSLE', 'R2_train', 'R2_test', 'Adj_R2_train', 'Adj_R2_test', 'params'],
                                       index = list(range(max_iterations)))
         
         for i in range(max_iterations):
@@ -95,7 +103,7 @@ class Trees:
             df_results['params'][i] = hyperparameters
             print("done")
             
-        df_results['delta_R2'] = abs(df_results["R2_train"]-df_results["R2_test"])
+        df_results['delta_RMSE'] = abs(df_results["RMSE train"]-df_results["RMSE"])
         # Sort with best score on top
         df_results.sort_values(['RMSE', 'R2_test'], ascending = [True, False], inplace = True)
         df_results.reset_index(inplace=True)
